7b7d699157bf578d2c82b2b327e8c8fa9fd240d7
Kgolagano POO <kpoo@c5r7s8.wethinkcode.co.za>
2019-05-24 16:32:08 +0200

    main & strcpy

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kpoo <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 11:29:48 by kpoo              #+#    #+#             */
-/*   Updated: 2019/05/23 13:40:54 by kpoo             ###   ########.fr       */
+/*   Updated: 2019/05/24 16:29:41 by kpoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
  
@@ -16,9 +16,8 @@
 
 int main(void)
 {
-	
 	/*
-	//(A) ft_strlen_testing - done
+	//(1) ft_strlen_testing - done
 	char *test;
 	test = "Coolie";
 	//printf("%zu\n", strlen(test));
@@ -26,7 +25,7 @@ int main(void)
 
 	printf("\n"); //nextline
 
-	//(B) ft_strcpy_testing - not done
+	//(2) ft_strcpy_testing - done
 	char *dest;
 	char *src;
 	dest = "food";
@@ -36,56 +35,61 @@ int main(void)
 
 	printf("\n"); //nextline
 
-	//(C) ft_isdigit_testing - done
+	//(3) ft_isdigit_testing - done
 	//printf("%d\n", isdigital(91));
 	//printf("%d\n", ft_isdigit(91));
 	
 	printf("\n"); //nextline
 
-	//(D) ft_isalpha - done
+	//(4) ft_isalpha - done
 	//printf("%d\n", isalpha(0));
 	//printf("%d\n", ft_isalpha(0));
 
 	printf("\n"); //nextline
 
-	//(E) ft_isalnum_testing - done
+	//(5) ft_isalnum_testing - done
 	//printf("%d\n", isalnum('A'));
 	//printf("%d\n", ft_isalnum('A'));
 
 	printf("\n"); //nextline
 
-	//(F) ft_isascii_testing - done
+	//(6) ft_isascii_testing - done
 	//printf("%d\n", isascii('$'));
 	//printf("%d\n", ft_isascii('$'));
 	
 	printf("\n"); //nextline
 
-	//(G) ft_isprint_testing - done
+	//(7) ft_isprint_testing - done
 	//printf("%d\n", isprint(' '));
 	//printf("%d\n", ft_isprint(' '));
 	
 	printf("\n"); //nextline
-	*/
-	//(H) ft_putchar_testing - done
-	/*
+
+	//(8) ft_putchar_testing - done
 	ft_putchar('C');
 
 	printf("\n"); //nextline
 
-	//(I) ft_putstr_testing - not done
+	//(9) ft_putstr_testing - not done
 	ft_putstr("kgolagano");
 
-	*/
-
-	//(J) ft_toupper_testing - done
+	//(10) ft_toupper_testing - done
 	printf("%c\n", ft_toupper('?'));
 
 
 	printf("\n"); //nextline
 
-	//(K) ft_tolower_testing - done
+	//(11) ft_tolower_testing - done
 	//printf("%c\n", ft_tolower('?'));
 
 	printf("\n"); //nextline
+
+	//(12) ft_memset_testing - done
+	char str[50] = "Testing";
+ 	printf("%s\n", ft_memset(str, '@', 3));
+*/	
+	//(13) ft_bzero_testing - not done
+	ft_bzero("cool", 2);
+
 	return (0);
 }
