37eb7d91dd0f4be4a863b51e0b7cf635f78e343b
Kgolagano POO <kpoo@c5r7s8.wethinkcode.co.za>
2019-05-29 12:41:59 +0200

    adding memcmp & strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpoo <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/29 12:29:05 by kpoo              #+#    #+#             */
+/*   Updated: 2019/05/29 12:40:01 by kpoo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	len2;
+
+	if (*needle == '\0')
+		return ((char *)haystack);
+	len2 = ft_strlen(needle);
+	while (*haystack != '\0' && len-- >= len2)
+	{
+		if (*haystack == *needle && ft_memcmp(haystack, needle, len2) == 0)
+			return ((char *)haystack);
+		haystack++;
+	}
+	return (NULL);
+}
