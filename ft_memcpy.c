e763804af32afd1d4431ae29436741514d3d4a59
Kgolagano POO <kpoo@c5r7s8.wethinkcode.co.za>
2019-06-06 14:41:55 +0200

    adding memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,24 +6,27 @@
 /*   By: kpoo <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 11:45:51 by kpoo              #+#    #+#             */
-/*   Updated: 2019/06/04 14:38:28 by kpoo             ###   ########.fr       */
+/*   Updated: 2019/06/06 14:41:23 by kpoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
+	char			*d;
+	const char		*s;
 	size_t			i;
-	unsigned char	*d;
-	unsigned char	*s;
 
-	d = (unsigned char *)dst;
-	s = (unsigned char *)src;
+	d = dst;
+	s = src;
 	i = 0;
 	while (i < n)
 	{
-		d[i] = s[i];
+		*d = *s;
+		d++;
+		s++;
 		i++;
 	}
 	return (dst);
